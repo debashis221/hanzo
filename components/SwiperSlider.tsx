d648bcf5437b16b56daa83f9f8a8b86d4e37bc1f
Debashis Saha <debashissaha221@gmail.com>
2023-03-07 12:09:45 +0530

    changes

--- components/SwiperSlider.tsx
+++ components/SwiperSlider.tsx
@@ -1,13 +1,27 @@
 'use client';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import { FavouriteList } from 'interfaces/interfaces';
 import Image from 'next/image';
+import { Autoplay, Pagination } from 'swiper';
 
 const SwiperSlider = ({ data }: { data: FavouriteList }) => {
   return (
-    <div className="px-5 my-5">
-      <Swiper slidesPerView={1}>
+    <div className="my-5 px-5">
+      <Swiper
+        slidesPerView={1}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Autoplay, Pagination]}
+      >
         {data?.data?.map((item) => {
           return (
             <>
@@ -24,7 +38,7 @@ const SwiperSlider = ({ data }: { data: FavouriteList }) => {
                     className="max-h-[63vh] rounded-lg object-cover blur-sm"
                     priority
                   />
-                  <div className="max-w-[40vw] text-center py-2 absolute bottom-10 left-10 flex flex-col rounded-md border border-primary bg-primary bg-opacity-30 bg-clip-padding backdrop-blur-lg backdrop-filter">
+                  <div className="absolute bottom-10 left-10 flex max-w-[40vw] flex-col rounded-md border border-primary bg-primary bg-opacity-30 bg-clip-padding py-2 text-center backdrop-blur-lg backdrop-filter">
                     <h2 className="px-5  text-2xl font-bold text-white">
                       {item.title_english}
                     </h2>
@@ -45,8 +59,10 @@ const SwiperSlider = ({ data }: { data: FavouriteList }) => {
                         {item.duration}
                       </h2>
                     </div>
-                    <p className='text-white px-5'>{item.synopsis.slice(0,200)}</p>
-                    <div className="flex gap-2 items-center justify-center">
+                    <p className="px-5 text-white">
+                      {item.synopsis.slice(0, 200)}
+                    </p>
+                    <div className="flex items-center justify-center gap-2">
                       <button className="btn-primary btn my-3 rounded-full">
                         Watch Now
                       </button>
